e703ac711c544782a9e3cdbf7fdf52ccf5f3ff6a
manuruiz5 <manuela.ruiz@correo.ucu.edu.uy>
2024-11-03 12:57:03 -0300

    rutas

---
+++ src/shared/services/CommentService.jsx
@@ -0,0 +1,39 @@
+import useApi from "../hooks/useApi";
+
+const useCommentService = () => {
+  const { doRequest } = useApi();
+
+  const createComment = async (postId, content) => {
+    const response = await doRequest(
+      `posts/${postId}/comments`,
+      "POST",
+      { content },
+      false 
+    );
+    return response;
+  };
+
+  const deleteComment = async (postId, commentId) => {
+    const response = await doRequest(
+      `posts/${postId}/comments/${commentId}`,
+      "DELETE",
+      null,
+      false 
+    );
+    return response;
+  };
+
+  const getComment = async (commentId) => {
+    const response = await doRequest(
+      `posts/comments/${commentId}`,
+      "GET",
+      null,
+      false 
+    );
+    return response;
+  };
+
+  return { createComment, deleteComment, getComment };
+};
+
+export default useCommentService;
