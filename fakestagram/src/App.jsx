446803fc10fd4a0e96bbd67e04cecb6ce85cca3a
Martín <martin.cabral@correo.ucu.edu.uy>
2024-11-03 23:38:14 -0300

    Bastante avanzado, habria que mergear y empezar a testear

--- fakestagram/src/App.jsx
+++ fakestagram/src/App.jsx
@@ -6,9 +6,14 @@ import FeedPage from 'features/feed/pages/FeedPage';
 import PrivateRoute from 'shared/navigation/PrivateRoute';
 import UnAuthRoute from 'shared/navigation/UnAuthRoute';
 import WIPPage from 'shared/pages/WIPPage';
+import VistaUsuario from 'features/auth/Profile/UserView';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+
+  function NotFoundPage(){
+    return <p>RIP</p>
+  }
   return (
     <AuthProvider>
       <BrowserRouter>
@@ -20,9 +25,11 @@ function App() {
             <Route path="/login/weba" element={<WIPPage />} />
             <Route path="/login/autogestion" element={<WIPPage />} />
           </Route>
+          <Route path="/profile/user" element={<VistaUsuario idUsuario="6719a9a0d3970a94da53e986" />} />
           <Route element={<PrivateRoute />}>
             <Route path="/" element={<FeedPage />} />
           </Route>
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
--- fakestagram/src/features/auth/Profile/UserView.jsx
+++ fakestagram/src/features/auth/Profile/UserView.jsx
@@ -1,121 +1,203 @@
-import React, { useEffect, useState } from "react";
-import useApi from "../hooks/useApi";
+import React, { useEffect, useState, useContext } from "react";
+import useApi from "../../../shared/hooks/useApi";
+import '../styles/Profile.css';
 
 function CambiarFoto({ usuario }) {
-    const { doRequest } = useApi();
-    const [selectedFile, setSelectedFile] = useState(null);
-  
-    const handleFileChange = (event) => {
-      setSelectedFile(event.target.files[0]);
-    };
-  
-    const handleUpload = async () => {
-      if (!selectedFile) return;
-  
-      const formData = new FormData();
-      formData.append("foto", selectedFile); 
-  
-      try {
-        await doRequest(`users/${usuario.id}/foto`, "PUT", formData, true);
-        alert("Foto de perfil actualizada con éxito.");
-      } catch (error) {
-        console.error("Error uploading profile picture:", error);
-      }
-    };
-  
-    return (
-      <div>
-        <img src={usuario.fotoUrl} alt="Foto de perfil" />
-        <input type="file" onChange={handleFileChange} />
-        <button className="cambiarFoto" onClick={handleUpload}>Cambiar</button>
-      </div>
-    );
-  }
-  
-
-function CambiarDescripcion(userId, newDescripcion) {
   const { doRequest } = useApi();
-  const [nuevaDescripcion, setDescripcion] = useState(null);
-  
-    const handleTextChange = (event) => {
-      setDescripcion(event.target.files[0]);
-    };
+  const [selectedFile, setSelectedFile] = useState(null);
 
-  handleUpload = async () => {
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) return;
+
+    const formData = new FormData();
+    formData.append("foto", selectedFile);
+
     try {
-      await doRequest(`users/${userId}/descripcion`, 'PUT', { descripcion: nuevaDescripcion }, true);
+      await doRequest(`user/profile/edit`, "PUT", formData, true);
+      alert("Foto de perfil actualizada con éxito.");
+    } catch (error) {
+      console.error("Error uploading profile picture:", error);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button className="cambiarFoto" onClick={handleUpload}>Cambiar</button>
+    </div>
+  );
+}
+
+function CambiarDescripcion({ userId }) {
+  const { doRequest } = useApi();
+  const [nuevaDescripcion, setDescripcion] = useState("");
+
+  const handleTextChange = (event) => {
+    setDescripcion(event.target.value);
+  };
+
+  const handleUpload = async () => {
+    try {
+      await doRequest(`user/profile/edit`, 'PUT', { descripcion: nuevaDescripcion }, true);
+      alert("Descripción actualizada con éxito.");
     } catch (error) {
       console.error("Error updating description:", error);
     }
   };
+
   return (
     <div>
-        <p>{usuario.descripcion}</p>
-        <input type="text" onChange={handleTextChange} />
-        <button className="cambiarDescripcion" onClick={handleUpload}>Cambiar</button>
-      </div>
-  )
+      <input type="text" placeholder="Nueva descripción" onChange={handleTextChange} />
+      <button className="cambiarDescripcion" onClick={handleUpload}>Cambiar</button>
+    </div>
+  );
 }
 
 function FotoPerfil({ usuario }) {
-  const cambiarFoto = CambiarFoto(usuario);
   return (
-    <div>
-      <img src={usuario.fotoUrl} alt="Foto de perfil" />
-      <button className="cambiarFoto" onClick={cambiarFoto}>Cambiar</button>
+    <div className="FotoPerfil">
+    <h1>{usuario.user.username}</h1>
+      <img src={usuario.profilePicture} alt="Foto de perfil" />
+      {usuario && usuario.user ? ( 
+        <p>{usuario.user.username}</p>
+      ) : (
+        <h2>Cargando...</h2> 
+      )}
+      <p>No tiene description el back</p>
     </div>
   );
 }
 
-function DatosUsuario({ usuario }) {
-  const cambiarDescripcion = CambiarDescripcion(usuario);
+//Aca tengo que chequear si el usuario soy yo mismo
+function DatosUsuario({ usuario, myProfile }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { doRequest } = useApi();
 
-  return (
-    <div>
-      <h2>{usuario.nombre}</h2>
-      <p>{usuario.descripcion}</p>
-      <button className="cambiarDescripcion" onClick={cambiarDescripcion}>
-        Cambiar Descripción
-      </button>
-    </div>
-  );
-}
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
-function Posts() {
-
-}
-
-function VistaUsuario({ IdUsuario }) {
-    const { user: currentUser } = useContext(AuthContext);
-    const { doRequest } = useApi();
-    const [usuario, setUsuario] = useState(null);
-
-    const isOwner = usuario && currentUser && usuario.id === currentUser.id;
-
-
-    if(!isOwner){
-        useEffect(() => {
-            const fetchUser = async () => {
-                try {
-                    const response = await doRequest(`profile/${IdUsuario}`, 'GET', null, true);
-                    setUsuario(response.data);
-                } catch (error) {
-                    console.error("Error fetching user data:", error);
-                }
-            };
-            fetchUser();
-        }, [IdUsuario, doRequest]); 
+  const handleAddFriend = async () => {
+    try {
+      await doRequest(`/add-friend/${usuario._id}`, "POST", null, true);
+      alert("Solicitud de amistad enviada.");
+    } catch (error) {
+      console.error("Error al añadir amigo:", error);
     }
-
-    return (
-        <div className="container">
-            <div className="DatosUsuario">
-                <FotoPerfil usuario={usuario}/>
-                <DatosUsuario usuario={usuario}/>
-                <Posts />
+  };
+  
+  const handleRemoveFriend = async () => {
+    try {
+      await doRequest(`/remove-friend/${usuario._id}`, "DELETE", null, true);
+      alert("Amigo eliminado correctamente.");
+    } catch (error) {
+      console.error("Error al eliminar amigo:", error);
+    }
+  };
+  
+  return (
+    <div className="Datos">
+      <div className="Friends">
+        <p className="InformacionBasica">Amigos: {usuario.user.friends.length}</p>
+        <p className="InformacionBasica">Posts: {usuario.posts.length}</p>
+      </div>
+      
+      {myProfile ? (
+        <>
+          <button className="editProfileButton" onClick={toggleModal}>
+            Editar perfil
+          </button>
+          {isModalOpen && (
+            <div className="modalOverlay">
+              <div className="modalContent">
+                <button className="closeModal" onClick={toggleModal}>X</button>
+                <CambiarFoto usuario={usuario} />
+                <CambiarDescripcion userId={usuario.user._id} />
+              </div>
             </div>
-        </div>
+          )}
+        </>
+      ) : (
+        usuario.user.friends.includes(usuario._id) ? (
+          <button className="removeFriendButton" onClick={handleRemoveFriend}>
+            Eliminar amigo
+          </button>
+        ) : (
+          <button className="addFriendButton" onClick={handleAddFriend}>
+            Añadir como amigo
+          </button>
+        )
+      )}
+    </div>
+  );
+}  
+
+
+function Posts({ usuario }) {
+  if (!usuario || !usuario.posts || usuario.posts.length === 0) {
+    return (
+      <div className="Posts">
+        <h1>No hay posts</h1>
+      </div>
     );
+  }
+  //Añadir onClick
+  return (
+    <div className="Posts"> 
+      {usuario.posts.map(post => (
+        <div key={post._id} className="Post">
+          <img src={post.imageUrl} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default VistaUsuario; 
+
+function VistaUsuario({ idUsuario }) {
+  const { doRequest } = useApi();
+  const [usuario, setUsuario] = useState({ user: { username: '', profilePicture: '' }, _id: '', posts: null });
+
+
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  const fetchUser = async () => {
+    try {
+      const response = await doRequest(`user/profile/${idUsuario}`, 'GET', null, true);
+      setUsuario(response.data);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  fetchUser();
+}, [idUsuario, doRequest]);
+
+const user = JSON.parse(localStorage.getItem('user'));
+let myProfile = false;
+{usuario && usuario.user
+if(user._id === usuario.user._id){
+  myProfile = true;
+}
+}
+return (
+  loading ? (
+    <div>Cargando...</div>
+  ) : (
+    <div className="container">
+      <div className="DatosUsuario">
+        <FotoPerfil usuario={usuario} />
+        <DatosUsuario usuario={usuario} myProfile={myProfile}/>
+      </div>
+      <Posts usuario={usuario} />
+    </div>
+  )
+);
+}
+export default VistaUsuario;
